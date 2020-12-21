f17bf2d0b97a029c3e4038a1ffd5ab2a09179d1c
Gregory Vial <gregory.vial@continental-corporation.com>
2020-12-21 21:23:40 +0100

    Working version

--- write.py
+++ write.py
@@ -12,19 +12,49 @@ import streamlit as st
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 
+from session_state import get
+
 URL = "http://127.0.0.1:5002/api/word"
 
+BLANK = (
+    "iVBORw0KGgoAAAANSUhEUgAAAPoAAAD6AQAAAACgl2eQAAAAOklEQVR4nO3KsQ"
+    + "0AMAzDMLf/P5XL0g+8F6BW8Wxqc/tPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD4"
+    + "FzzqWAO0JciZyAAAAABJRU5ErkJggg=="
+)
 
-def submit_img(img, lang="EN", URL=URL):
+
+def submit_img(img, lang="en", url=URL):
     """Submit a b64 encoded image and get letter back."""
-    myobj = {"img": img, "word": "", "lang": "en", "nb_output": 1}
-    x = requests.post(URL, data=json.dumps(myobj))
-    return x.text
+    if img == BLANK:
+        return json.dumps(json.dumps({"word": None}))
+    myobj = {"img": img, "word": "", "lang": lang, "nb_output": 1}
+    req = requests.post(url, data=json.dumps(myobj))
+    return req.text
 
 
-def rgb2gray(rgb):
-    """RGB image to grayscale."""
-    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+def img2b64(img):
+    """Converts a color image to black&white b64."""
+    img = img[..., :3]
+    # img = rgb2gray(img)
+    img = img.astype(np.uint8)
+
+    pil_img = Image.fromarray(img)
+    pil_img = pil_img.convert("1")
+    buff = BytesIO()
+    pil_img.save(buff, format="PNG")
+
+    return base64.b64encode(buff.getvalue()).decode("utf-8")
+
+
+# def rgb2gray(rgb):
+#    """RGB image to grayscale."""
+#    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+def get_letter():
+    """Choose a random upper case letter."""
+    letter = random.choice(string.ascii_uppercase)
+    return letter
 
 
 # with open('text.json') as json_file:
@@ -35,48 +65,62 @@ def rgb2gray(rgb):
 # stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 
 
-@st.cache(ttl=5)
-def get_letter():
-    letter = random.choice(string.ascii_uppercase)
-    return letter
+if __name__ == "__main__":
+    # Session initialization
+    session = get(x=1, letter="", successes=0)
 
+    # Draw page
+    # Create canva
+    CanvasResult = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",
+        stroke_width=10,
+        stroke_color="#D01111",  # "#000",
+        background_color="#fff",
+        background_image=None,
+        update_streamlit=True,
+        height=250,
+        width=250,
+        drawing_mode="freedraw",
+        key="canvas",
+    )
 
-letter = get_letter()
-st.text("Write the letter {}".format(letter))
+    # Empty slot for expected letter
+    res_text = st.empty()
 
+    # Next letter button
+    change = st.button("Next letter")
 
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=5,
-    stroke_color="#D01111",  # "#000",
-    background_color="#fff",
-    background_image=None,
-    update_streamlit=True,
-    height=250,
-    width=250,
-    drawing_mode="freedraw",
-    key="canvas",
-)
+    # Score
+    score = st.empty()
 
-erase = st.button("Erase")
-submit = st.button("Submit")
+    # Logical flow
+    # Get letter to be drawn
+    if session.x == 1:
+        session.x += 1
+        session.letter = get_letter()
+    elif change:
+        session.letter = get_letter()
 
-if canvas_result.image_data is not None:
-    img = canvas_result.image_data[..., :3]
-    img = rgb2gray(img)
-    img = img.astype(np.uint8)
+    # Display letter
+    res_text.text("Please draw a {}".format(session.letter))
 
-    pil_img = Image.fromarray(img)
-    buff = BytesIO()
-    pil_img.save(buff, format="PNG")
+    # Display score
+    if session.successes:
+        score.write("You correctly draw {} letters".format(session.successes))
 
-    b64img = base64.b64encode(buff.getvalue()).decode("utf-8")
+    # Process drawing
+    if CanvasResult.image_data is not None:
+        b64img = img2b64(CanvasResult.image_data)
+        js = json.loads(json.loads(submit_img(b64img)))
+        user_letter = js["word"]
 
-    # Need to check why double json loads is required!
-    js = json.loads(json.loads(submit_img(b64img)))
-    res = js["word"]
-
-    st.text("Letter found: {}, expected: {}".format(res, letter))
-    if res == letter:
-
-        st.balloons()
+        if user_letter is None:
+            st.stop()
+        else:
+            res_text.text(
+                "Letter found: {}, expected: {}".format(user_letter, session.letter)
+            )
+            if user_letter == session.letter:
+                st.balloons()
+                session.successes += 1
+                score.write("You correctly draw {} letters".format(session.successes))
